d2d92cd392ff6aa5738e772caf3a6423420a94ad
kaivanov8 <kosiv6799@gmail.com>
2024-11-11 21:35:54 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+# для module_4_1
+
+from fake_match import divide as m1
+from true_match import divide as m2
+result1 = m1(69,3)
+result2 = m1(3,0)
+result3 = m2(49,7)
+result4 = m2(15,0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
